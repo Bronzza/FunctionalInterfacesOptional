646f6af881cf4e4069f84d49221f9103fbc51766
Sergey Bronzza <repinetskiy@gmail.com>
2019-03-10 17:50:58 +0200

    Commit after correction to secondBranch

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -8,21 +8,22 @@ import java.util.stream.Stream;
 
 public class Calculator {
 
+    final String message = "Incorrect sybmol, enter: +, -, *, ^, #, !";
+    final private Map<Character, BinaryOperator<Double>> operations = new HashMap<>() {
+        {
+            operations.put('+', (numb, numb2) -> numb + numb2);
+            operations.put('-', (numb, numb2) -> numb - numb2);
+            operations.put('*', (numb, numb2) -> numb * numb2);
+            operations.put('/', (numb, numb2) -> numb / +numb2);
+            operations.put('^', (numb, numb2) -> Math.pow(numb, numb2));
+            operations.put('#', (numb, numb2) -> Math.pow(numb, 1 / numb2));
+            operations.put('!', (numb, numb2) -> Math.pow(numb, numb2) / (numb + 117));
+        }
+    };
+
     public double calculate(char operation, double number1, double number2) {
-
-        Map<Character, BinaryOperator<Double>> operations = new HashMap<>();
-        operations.put('+', (numb, numb2) -> numb + numb2);
-        operations.put('-', (numb, numb2) -> numb - numb2);
-        operations.put('*', (numb, numb2) -> numb * numb2);
-        operations.put('/', (numb, numb2) -> numb / +numb2);
-        operations.put('^', (numb, numb2) -> Math.pow(numb, numb2));
-        operations.put('#', (numb, numb2) -> Math.pow(numb, 1 / numb2));
-        operations.put('!', (numb, numb2) -> Math.pow(numb, numb2) / (numb + 117));
-
-
         return Optional.ofNullable(operations.get(operation))
-                .orElseThrow(() -> new IllegalArgumentException("Incorrect sybmol, enter: +, -, *, " +
-                        "/, ^, #, !"))
+                .orElseThrow(() -> new IllegalArgumentException(message))
                 .apply(number1, number2);
     }
 
